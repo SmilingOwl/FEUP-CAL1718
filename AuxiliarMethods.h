91ba8246d9329dd5f7a7040a85dd35f266f66c71
Unknown <julianamcm05@gmail.com>
2018-03-28 16:46:58 +0100

    added read text files functions

--- AuxiliarMethods.h
+++ AuxiliarMethods.h
@@ -1,69 +1,127 @@
 #include "Graph.h"
 #include <cmath>
-
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-class AuxiliarMethods{
+string NodesFileName;
+string EdgesFileName;
+string StreetsFileName;
+
+class AuxiliarMethods {
 public:
-	double getDistance(double lat1, double lon1,double lat2,double lon2);
+	double getDistance(double lat1, double lon1, double lat2, double lon2);
 	double getTime(double distance, double velocity);
 	double deg2rad(double deg);
+
+	bool openFile(ifstream &file, const string fileName);
 	template<class T>
-	void extractData(string fileName, T &g);
+	void extractDataNodes(T &g);
+	template<class T>
+	void extractDataArestas(T &g);
+	template<class T>
+	void extractDataStreets(T &g);
 };
 
-
-
-
 //Haversine Formula (kilometers)
-double AuxiliarMethods::getDistance(double lat1, double lon1,double lat2,double lon2) {
-  double R = 6371; // Radius of the earth in km
-  double dLat = deg2rad(lat2-lat1);
-  double dLon = deg2rad(lon2-lon1);
-  double a = sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon/2) * sin(dLon/2);
-  double c = 2 * atan2(sqrt(a), sqrt(1-a));
-  double d = R * c; // Distance in km
-  return d;
+double AuxiliarMethods::getDistance(double lat1, double lon1, double lat2,
+		double lon2) {
+	double R = 6371; // Radius of the earth in km
+	double dLat = deg2rad(lat2 - lat1);
+	double dLon = deg2rad(lon2 - lon1);
+	double a = sin(dLat / 2) * sin(dLat / 2)
+			+ cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon / 2)
+					* sin(dLon / 2);
+	double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+	double d = R * c; // Distance in km
+	return d;
 }
 
-double AuxiliarMethods::getTime(double distance, double velocity){
-	return distance/velocity;
+double AuxiliarMethods::getTime(double distance, double velocity) {
+	return distance / velocity;
 }
 
 double AuxiliarMethods::deg2rad(double deg) {
-  return deg * (M_PI/180);
+	return deg * (M_PI / 180);
 }
 
+bool AuxiliarMethods::openFile(ifstream &file, const string fileName) {
+	file.open(fileName);
 
-/*
+	if (!file)
+		cerr << "Error" << endl;
+	else
+		return true;
+}
 
 template<class T>
-void AuxiliarMethods::extractData(string fileName, T &g)
-{
-	stringstream ssA;
-	ifstream file(fileName);
-	int ID;
+void AuxiliarMethods::extractDataNodes(T &g) {
+
+	ifstream file;
+	openFile(file, NodesFileName);
+
+	string line;
+
+	int ID = 0;
 	double latitude;
 	double longitude;
+	string string;
 
-	if(file.is_open())
-	{
-		string line;
+	while (getline(file, line)) {
+		stringstream lineSs(line);
 
-		while(getline(file,line))
-		{
-			//ID
-			getline(ssA,ID,';');
-			//Latitude
-			getline(ssA,latitude,';');
-			//longitude
-			getline(ssA,longitude,';');
+		//ID
+		lineSs >> ID;
+		getline(lineSs, string, ';');
+		//Latitude
+		lineSs >> latitude;
+		getline(lineSs, string, ';');
+		//longitude
+		lineSs >> longitude;
 
-			Vertex temV=new Vertex(ID,latitude,longitude);
-			g.addVertex(tempV);
-
-		}
+		g.addNode(ID, latitude, longitude);
 	}
+	file.close();
 }
-*/
+
+template<class T>
+void AuxiliarMethods::extractDataArestas(T &g) {
+	ifstream file;
+	openFile(file, EdgesFileName);
+
+	string line;
+
+	int idEdge = 0;
+	int idOrigin = 0;
+	int idFinal = 0;
+
+	while (getline(file, line)) {
+		stringstream lineSs(line);
+		string string;
+		//ID
+		lineSs >> idEdge;
+		getline(lineSs, string, ';');
+		//Origem
+		lineSs >> idOrigin;
+		getline(lineSs, string, ';');
+		//Destino
+		lineSs >> idFinal;
+		g.addEdge(idEdge, idOrigin, idFinal);
+	}
+	file.close();
+}
+
+template<class T>
+void AuxiliarMethods::extractDataStreets(T &g) {
+	ifstream file;
+	openFile(file, StreetsFileName);
+	string line;
+
+	while(getline(file,line))
+	{
+		stringstream lineSs(line);
+
+	}
+
+}
